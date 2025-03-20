2dc47ce2b72e0c5c87f3892ebf78fb99f56bbceb
Gus <g.92oliveira@gmail.com>
2025-03-20 06:20:42 -0300

    start

---
+++ src/main/java/com/example/admin_catalog/AdminCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.example.admin_catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminCatalogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/admin_catalog/AdminCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.admin_catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
