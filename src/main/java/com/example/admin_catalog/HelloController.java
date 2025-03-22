e8e9bd7df66d3e89cac23f2e3eb94f520f2e8053
Gus <g.92oliveira@gmail.com>
2025-03-22 06:37:25 -0300

    config maps and secrets

--- src/main/java/com/example/admin_catalog/HelloController.java
+++ src/main/java/com/example/admin_catalog/HelloController.java
@@ -2,16 +2,36 @@ package com.example.admin_catalog;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.IOException;
 
 @RestController
 public class HelloController {
-    
+
     @GetMapping("/")
     public String hi() {
         String name = System.getenv("NAME");
         String age = System.getenv("AGE");
         return "Hello, I am " + name + "I`m " + age;
     }
-    
+
+    @GetMapping("/secrets")
+    public String secrets() {
+        String name = System.getenv("USER");
+        String age = System.getenv("PASSWORD");
+        return "Hello, I am " + name + "I`m " + age;
+    }
+
+    @GetMapping("/configmap")
+    public String configMap() {
+        try {
+            String content = new String(Files.readAllBytes(Paths.get("myFamly/family.txt")));
+            return content;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "Error reading file";
+        }
+    }
+
 }
